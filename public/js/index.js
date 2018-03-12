81b7478bf935a50fc13a12981cebabb8e9de8c76
ChauDing <1603661@rgu.ac.uk>
2018-03-12 20:14:29 +0000

    keep me us basic look with running js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,13 @@
-
-    $('.ui.sidebar').sidebar('setting','closeable',false).sidebar('attach events', '#burger-menu');
-    $(".remove").off('click').on('click', function(){
-        $('.ui.sidebar') .sidebar('hide');
-    });
-    $('.ui.dropdown').dropdown();
+$(document).ready(function(){
+  $('.ui.sidebar').sidebar('setting','closeable',false).sidebar('attach events', '#burger');
+      $(".remove").off('click').on('click', function(){
+          $('.ui.sidebar') .sidebar('hide');
+      });
+      $('.ui.dropdown').dropdown();
+      $("#burger").on('click', function() {
+        console.log("YAY");
+      })
+      $(".header-search .prompt").on("click", function(){
+        alert("not implemented yet, sorry!");
+      })
+  });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var handlebars = require('handlebars');
 //get homgpage
 router.get('/', function(req, res){
   res.render('index');
 });
+
 module.exports = router;
