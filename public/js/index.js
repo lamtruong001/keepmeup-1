7a7d57bf8d8db3c80ff4264bd5db7d2b57c7eb0d
ChauDing <1603661@rgu.ac.uk>
2018-03-12 15:50:09 +0000

    public folder directory change

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,6 @@
-var Index = function(){};
 
-Index.load = function(){
     $('.ui.sidebar').sidebar('setting','closeable',false).sidebar('attach events', '#burger-menu');
     $(".remove").off('click').on('click', function(){
         $('.ui.sidebar') .sidebar('hide');
     });
     $('.ui.dropdown').dropdown();
-};
