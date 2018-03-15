0a56726bc1029b92937c19f6cc3b4c1208799d1f
ChauDing <1603661@rgu.ac.uk>
2018-03-15 10:19:13 +0000

    mongo files

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ var handlebars = require('handlebars');
 router.get('/', function(req, res){
   res.render('index');
 });
-
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/users/login');
+	}
+}
 module.exports = router;
