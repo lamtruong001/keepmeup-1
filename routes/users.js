37ed3409caa2a16966fe4bfd00f11ebf7b9c24d9
ChauDing <1603661@rgu.ac.uk>
2018-03-29 10:11:59 +0100

    login works

--- routes/users.js
+++ routes/users.js
@@ -17,14 +17,14 @@ router.get('/login', function(req, res){
 
 // Register User
 router.post('/register', function(req, res){
-	var fullName = req.body.fullName;
+	var name = req.body.name;
 	var email = req.body.email;
 	var username = req.body.username;
 	var password = req.body.password;
 	var password2 = req.body.password2;
 	console.log(JSON.stringify(req.body));
 	// Validation
-	req.checkBody('fullName', 'Name is required').notEmpty();
+	req.checkBody('name', 'Name is required').notEmpty();
 	req.checkBody('email', 'Email is required').notEmpty();
 	req.checkBody('email', 'Email is not valid').isEmail();
 	req.checkBody('username', 'Username is required').notEmpty();
@@ -39,7 +39,7 @@ router.post('/register', function(req, res){
 		});
 	} else {
 		var newUser = new User({
-			fullName: fullName,
+			name: name,
 			email:email,
 			username: username,
 			password: password
@@ -54,13 +54,6 @@ router.post('/register', function(req, res){
 
 		res.redirect('/login');
 	}
-});
-router.get('/logout', function(req, res){
-	req.logout();
-
-	req.flash('success_msg', 'You are logged out');
-
-	res.redirect('/users/login');
 });
 
 passport.use(new LocalStrategy(
@@ -97,5 +90,12 @@ router.post('/login',
   function(req, res) {
     res.redirect('/');
   });
+	router.get('/logout', function(req, res){
+		req.logout();
+
+		req.flash('success_msg', 'You are logged out');
+
+		res.redirect('/login');
+	});
 
 module.exports = router;
